ea54657b025c41eb42019796d529e1e0134b01d2
realTristan <heytristaann@gmail.com>
2023-04-16 15:55:31 -0400

    cleaned up

--- fts.go
+++ fts.go
@@ -28,10 +28,10 @@ func InitJson(file string) *FTS {
 	}
 
 	// Load the json data
-	fts._ReadJson(file)
+	fts.readJson(file)
 
 	// Load the cache
-	fts._LoadCacheJson(fts.json)
+	fts.loadCacheJson(fts.json)
 
 	// Return the cache
 	return fts
@@ -41,11 +41,11 @@ func InitJson(file string) *FTS {
 func (fts *FTS) Set(key string, value map[string]string) {
 	fts.mutex.Lock()
 	defer fts.mutex.Unlock()
-	fts._Set(key, value)
+	fts.set(key, value)
 }
 
 // Set a value in the cache
-func (fts *FTS) _Set(key string, value map[string]string) {
+func (fts *FTS) set(key string, value map[string]string) {
 	fts.json = append(fts.json, value)
 	// Loop through the value
 	for _, v := range value {
@@ -54,7 +54,7 @@ func (fts *FTS) _Set(key string, value map[string]string) {
 			switch {
 			case len(word) <= 1:
 				continue
-			case !_IsAlphaNum(word):
+			case !isAlphaNum(word):
 				fmt.Println("skipping word:", word, "from:", value, "reason: not alphanumeric")
 				continue
 			}
@@ -72,11 +72,11 @@ func (fts *FTS) _Set(key string, value map[string]string) {
 func (fts *FTS) Clean() {
 	fts.mutex.Lock()
 	defer fts.mutex.Unlock()
-	fts._Clean()
+	fts.clean()
 }
 
 // Clean the FTS cache
-func (fts *FTS) _Clean() {
+func (fts *FTS) clean() {
 	fts.cache = map[string][]int{}
 	fts.keys = []string{}
 	fts.json = []map[string]string{}
@@ -86,26 +86,26 @@ func (fts *FTS) _Clean() {
 func (fts *FTS) Reset(fileName string) {
 	fts.mutex.Lock()
 	defer fts.mutex.Unlock()
-	fts._Reset(fileName)
+	fts.reset(fileName)
 }
 
 // Reset the FTS cache
-func (fts *FTS) _Reset(fileName string) {
+func (fts *FTS) reset(fileName string) {
 	fts.cache = map[string][]int{}
 	fts.keys = []string{}
 	fts.json = []map[string]string{}
-	fts._ReadJson(fileName)
-	fts._LoadCacheJson(fts.json)
+	fts.readJson(fileName)
+	fts.loadCacheJson(fts.json)
 }
 
 // Read the json data
-func (fts *FTS) _ReadJson(fileName string) {
+func (fts *FTS) readJson(fileName string) {
 	var data, _ = os.ReadFile(fileName)
 	json.Unmarshal(data, &fts.json)
 }
 
 // Load the FTS cache
-func (fts *FTS) _LoadCacheJson(json []map[string]string) error {
+func (fts *FTS) loadCacheJson(json []map[string]string) error {
 	// Loop through the json data
 	for i, item := range json {
 		if reflect.TypeOf(item).Kind() != reflect.Map {
@@ -119,7 +119,7 @@ func (fts *FTS) _LoadCacheJson(json []map[string]string) error {
 
 			// Clean the value
 			value = strings.TrimSpace(value)
-			value = _RemoveDoubleSpaces(value)
+			value = removeDoubleSpaces(value)
 			value = strings.ToLower(value)
 
 			// Loop through the words
@@ -127,7 +127,7 @@ func (fts *FTS) _LoadCacheJson(json []map[string]string) error {
 				switch {
 				case len(word) <= 1:
 					continue
-				case !_IsAlphaNum(word):
+				case !isAlphaNum(word):
 					fmt.Println("skipping word:", word, "from:", value, "reason: not alphanumeric")
 					continue
 				}
@@ -136,7 +136,7 @@ func (fts *FTS) _LoadCacheJson(json []map[string]string) error {
 					fts.keys = append(fts.keys, word)
 					continue
 				}
-				if !_ContainsInt(fts.cache[word], i) {
+				if !containsInt(fts.cache[word], i) {
 					continue
 				}
 				fts.cache[word] = append(fts.cache[word], i)
