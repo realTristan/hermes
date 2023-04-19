56723e42ed68a1e1e1b4f1fec76cc38f7311d6f6
realTristan <heytristaann@gmail.com>
2023-04-19 10:56:11 -0400

    cleaned up

--- cache/testing/cache_funcs/cache_funcs.go
+++ cache/testing/cache_funcs/cache_funcs.go
@@ -25,43 +25,30 @@ func main() {
 		fmt.Println(err)
 	}
 
-	// Track start time
-	var startTime time.Time = time.Now()
-
 	// The data for the user_id key
 	var data = map[string]interface{}{
-		"name":       "tristan",
-		"age":        17,
-		"expiration": time.Now(),
+		"name": "tristan",
+		"age":  17,
 	}
 
 	// Set the value in the cache
-	if err := cache.Set("user_id", data); err != nil {
-		fmt.Println(err)
-	}
-
-	// Print duration
-	fmt.Printf("Set user_id in %s\n", time.Since(startTime))
-
-	// Track start time
-	startTime = time.Now()
+	duration("Set", func() {
+		if err := cache.Set("user_id", data); err != nil {
+			fmt.Println(err)
+		}
+	})
 
 	// Get the user_id value
-	var user = cache.Get("user_id")
-
-	// Check if the user is expired
-	if user["expiration"].(time.Time).Before(time.Now()) {
-		fmt.Println("Expired")
-	}
-
-	// Print duration
-	fmt.Printf("Got %v in %s\n", user, time.Since(startTime))
-
-	// Track start time
-	startTime = time.Now()
+	duration("Get", func() {
+		var user = cache.Get("user_id")
+		fmt.Println(user)
+	})
 
 	// Search for a word in the cache
-	var result = cache.FT.SearchOne("tristan", 100, false)
+	duration("Search", func() {
+		var result = cache.FT.SearchOne("tristan", 100, false)
+		fmt.Println(result)
+	})
 
 	/* Reset the FT cache
 	if err := cache.ResetFT(maxWords, maxSizeBytes, schema); err != nil {
@@ -69,15 +56,20 @@ func main() {
 	}*/
 
 	// Delete the user_id key
-	cache.Delete("user_id")
+	duration("Delete", func() {
+		cache.Delete("user_id")
+	})
 
 	// Search for a word in the cache
-	result = cache.FT.SearchOne("tristan", 100, false)
+	duration("Search", func() {
+		var result = cache.FT.SearchOne("tristan", 100, false)
+		fmt.Println(result)
+	})
+}
 
-	// Print the result
-	fmt.Println(result)
-
-	// Print result
-	fmt.Printf("Found %d results in %s\n", len(result), time.Since(startTime))
-	fmt.Println(result)
+// Track the duration of a function
+func duration(key string, f func()) {
+	var start time.Time = time.Now()
+	f()
+	fmt.Printf("\nExecution Duration for %s: %s\n", key, time.Since(start))
 }
