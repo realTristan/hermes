93d26905462c3ddb1b7697dea0eeaa76ede6fd6a
realTristan <heytristaann@gmail.com>
2023-04-18 21:07:13 -0400

    added more error security

--- cache/fulltext.go
+++ cache/fulltext.go
@@ -22,6 +22,10 @@ type FullText struct {
 func (c *Cache) InitFTJson(file string, maxWords int, maxSizeBytes int, schema map[string]bool) error {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
+	if c.FT != nil {
+		c.FT.mutex.Lock()
+		defer c.FT.mutex.Unlock()
+	}
 	return c.initFTJson(file, maxWords, maxSizeBytes, schema)
 }
 
@@ -40,7 +44,7 @@ func (c *Cache) initFTJson(file string, maxWords int, maxSizeBytes int, schema m
 		data:          map[string]map[string]interface{}{},
 		maxWords:      maxWords,
 		maxSizeBytes:  maxSizeBytes,
-		isInitialized: true,
+		isInitialized: false,
 	}
 
 	// Read the json
@@ -53,13 +57,21 @@ func (c *Cache) initFTJson(file string, maxWords int, maxSizeBytes int, schema m
 	// Iterate over the regular cache data and
 	// add it to the FT cache data
 	for k, v := range c.data {
+		if _, ok := c.FT.data[k]; ok {
+			c.FT.clean()
+			return fmt.Errorf("the key: %s has already been imported via json file. unable to add it to the cache", k)
+		}
 		c.FT.data[k] = v
 	}
 
 	// Load the cache data
 	if err := c.FT.loadCacheData(c.FT.data, schema); err != nil {
+		c.FT.clean()
 		return err
 	}
+
+	// Set the FT cache as initialized
+	c.FT.isInitialized = true
 
 	// Return the cache
 	return nil
@@ -69,6 +81,10 @@ func (c *Cache) initFTJson(file string, maxWords int, maxSizeBytes int, schema m
 func (c *Cache) InitFT(maxWords int, maxSizeBytes int, schema map[string]bool) error {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
+	if c.FT != nil {
+		c.FT.mutex.Lock()
+		defer c.FT.mutex.Unlock()
+	}
 	return c.initFT(maxWords, maxSizeBytes, schema)
 }
 
@@ -79,21 +95,33 @@ func (c *Cache) initFT(maxWords int, maxSizeBytes int, schema map[string]bool) e
 		return fmt.Errorf("full text cache already initialized")
 	}
 
-	// Convert the cache data into an array of maps
-	var data map[string]map[string]interface{} = map[string]map[string]interface{}{}
-	for k, v := range c.data {
-		data[k] = v
-	}
+	// Initialize the FT struct
 	c.FT = &FullText{
 		mutex:         &sync.RWMutex{},
 		cache:         map[string][]string{},
 		words:         []string{},
-		data:          data,
+		data:          map[string]map[string]interface{}{},
 		maxWords:      maxWords,
 		maxSizeBytes:  maxSizeBytes,
-		isInitialized: true,
+		isInitialized: false,
 	}
-	return c.FT.loadCacheData(data, schema)
+
+	// Convert the cache data into an array of maps
+	for k, v := range c.data {
+		c.FT.data[k] = v
+	}
+
+	// Load the cache data
+	if err := c.FT.loadCacheData(c.FT.data, schema); err != nil {
+		c.FT.clean()
+		return err
+	}
+
+	// Set the FT cache as initialized
+	c.FT.isInitialized = true
+
+	// Return the cache
+	return nil
 }
 
 // Reset the FullText cache with Mutex Locking
