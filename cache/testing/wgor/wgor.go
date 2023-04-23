5053505e08fc89758341b6f57f83da0ba0bc8dae
tristan <heytristaann@gmail.com>
2023-04-23 11:43:30 -0400

    tmp

---
+++ cache/testing/wgor/wgor.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	Hermes "github.com/realTristan/Hermes/cache"
+)
+
+func main() {
+	// Initialize the cache
+	var cache *Hermes.Cache = Hermes.InitCache()
+
+	// Initialize the FT cache with a json file
+	cache.FTInitWithJson("../../../data/data_indices.json", -1, -1, map[string]bool{
+		"id":             false,
+		"components":     false,
+		"units":          false,
+		"description":    true,
+		"name":           true,
+		"pre_requisites": true,
+		"title":          true,
+	})
+
+	// Test the WGOR function
+	var start = time.Now()
+	var data = cache.WGOR_SearchOneWord_Example()
+	var end = time.Since(start)
+
+	// Print the results
+	fmt.Println("Hermes: Average time is: ", end.Nanoseconds(), "ns or", end.Milliseconds(), "ms")
+	fmt.Println("Hermes: Total results: ", len(data))
+}
