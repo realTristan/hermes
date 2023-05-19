212cd1ca3015cf0220c6923d43530fd47f641320
Tristan <75189508+realTristan@users.noreply.github.com>
2023-05-19 07:00:59 -0400

    Update router.go

--- server/ws/router.go
+++ server/ws/router.go
@@ -7,36 +7,6 @@ import (
 	"github.com/realTristan/Hermes/server/ws/handlers"
 )
 
-/*
-
-app.Get("/values", handlers.Values(cache))
-	app.Get("/length", handlers.Length(cache))
-	app.Post("/clean", handlers.Clean(cache))
-	app.Post("/set", handlers.Set(cache))
-	app.Delete("/delete", handlers.Delete(cache))
-	app.Get("/get", handlers.Get(cache))
-	app.Get("/get/all", handlers.GetAll(cache))
-	app.Get("/keys", handlers.Keys(cache))
-	app.Get("/info", handlers.Info(cache))
-	app.Get("/exists", handlers.Exists(cache))
-
-	// Full-text Cache Handlers
-	app.Post("/ft/init", handlers.FTInit(cache))
-	app.Post("/ft/init/json", handlers.FTInitJson(cache))
-	app.Post("/ft/clean", handlers.FTClean(cache))
-	app.Get("/ft/search", handlers.Search(cache))
-	app.Get("/ft/search/oneword", handlers.SearchOneWord(cache))
-	app.Get("/ft/search/values", handlers.SearchValues(cache))
-	app.Get("/ft/search/withkey", handlers.SearchWithKey(cache))
-	app.Post("/ft/maxbytes", handlers.FTSetMaxBytes(cache))
-	app.Post("/ft/maxlength", handlers.FTSetMaxLength(cache))
-	app.Get("/ft/storage", handlers.FTStorage(cache))
-	app.Get("/ft/storage/size", handlers.FTStorageSize(cache))
-	app.Get("/ft/storage/length", handlers.FTStorageLength(cache))
-	app.Get("/ft/isinitialized", handlers.FTIsInitialized(cache))
-	app.Post("/ft/indices/sequence", handlers.FTSequenceIndices(cache))
-*/
-
 // Map of functions that can be called from the client
 var functions = map[string]func(*Hermes.Cache, *websocket.Conn) error{
 	"length":              handlers.Length,
