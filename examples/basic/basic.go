0f2cd09f235cf70ed6877c209de5fb491ee38f01
tristan <heytristaann@gmail.com>
2023-07-19 11:37:10 -0400

    fix example

--- examples/basic/basic.go
+++ examples/basic/basic.go
@@ -8,11 +8,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"net/http"
-	"strconv"
-	"time"
 
 	hermes "github.com/realTristan/hermes"
 )
@@ -20,57 +16,16 @@ import (
 // Initialize the cache
 var cache *hermes.Cache = hermes.InitCache()
 
-// Main function
 func main() {
-	// Initialize the FT cache with a json file
+	// Initialize the full-text cache
 	cache.FTInitWithJson("../../testing/data/data_hash.json", -1, -1, 3)
 
-	// Print host
-	fmt.Println(" >> Listening on: http://localhost:8000/")
-
-	// Listen and serve on port 8000
-	http.HandleFunc("/courses", Handler)
-	http.ListenAndServe(":8000", nil)
-}
-
-// Handle the incoming http request
-func Handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	// Get the query parameter
-	var query string = "CS"
-	if _query := r.URL.Query().Get("q"); _query != "" {
-		query = _query
-	}
-
-	// Get the limit parameter
-	var limit int = 100
-	if _limit := r.URL.Query().Get("limit"); _limit != "" {
-		limit, _ = strconv.Atoi(_limit)
-	}
-
-	// Get the strict parameter
-	var strict bool = false
-	if _strict := r.URL.Query().Get("strict"); _strict != "" {
-		strict, _ = strconv.ParseBool(_strict)
-	}
-
-	// Track the start time
-	var start time.Time = time.Now()
-
 	// Search for a word in the cache
-	// Make sure the show which keys you do want to search through,
-	// and which ones you don't
 	var res, _ = cache.Search(hermes.SearchParams{
-		Query:  query,
-		Limit:  limit,
-		Strict: strict,
+		Query:  "tristan",
+		Limit:  100,
+		Strict: false,
 	})
 
-	// Print the duration
-	fmt.Printf("\nFound %v results in %v", len(res), time.Since(start))
-
-	// Write the courses to the json response
-	var response, _ = json.Marshal(res)
-	w.Write(response)
+	fmt.Println(res)
 }
